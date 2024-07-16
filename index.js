6449e0170cc258631bba05d08220136ddcda6c0d
hemantp88 <panchalhemant030700@gmail.com>
2024-07-16 16:52:58 +0530

    login with session demo

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const User = require('./models/user');
 const { default: mongoose } = require('mongoose');
 const bcrypt = require('bcrypt');
-
+const session = require('express-session');
 
 mongoose.connect('mongodb://127.0.0.1:27017/loginDemo')
     .then(() => {
@@ -14,12 +14,15 @@ mongoose.connect('mongodb://127.0.0.1:27017/loginDemo')
     })
 
 
+
 app.set('view engine', 'ejs')//setting up ejs engine
 app.set('views', 'views');
 
 
 
 app.use(express.urlencoded({ extended: true }));//for getting body and param from url
+
+app.use(session({ secret: 'notagoodsecret' }))
 
 
 app.get('/', (req, res) => {
@@ -35,12 +38,11 @@ app.post('/register', async (req, res) => {
 
     const user = new User({ username, password: hashedpassword });
     await user.save();
+    req.session.user_id = user._id;
     res.redirect('/');
 })
 
-app.get('/secret', (req, res) => {
-    res.send('This is secret!');
-})
+
 
 app.get('/login', (req, res) => {
     res.render("login");
@@ -49,15 +51,29 @@ app.post('/login', async (req, res) => {
     // res.send(req.body);
     const { username, password } = req.body;
     const user = await User.findOne({ username: username });
+    if (!user) {
+        // res.send("try again incorrect username");
+        res.redirect('/login')
+    }
     const validPassword = await bcrypt.compare(password, user.password)
     if (validPassword) {
-        res.send("Yay welcome   !!!");
+        req.session.user_id = user._id;
+        // res.send("Yay welcome   !!!");
+        res.redirect('/secret')
+
     }
     else {
-        res.send("Try again");
+        // res.send("Try again");
+        res.redirect('/login');
     }
 })
 
+app.get('/secret', (req, res) => {
+    if (!req.session.user_id) {
+        res.redirect("/login")
+    }
+    res.send('This is secret!');
+})
 
 app.listen(3000, () => {
     console.log("Listening on port 3000");
